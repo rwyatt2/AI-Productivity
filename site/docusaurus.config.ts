8128caca7f16a02a5bee0867410a52d69f26f8e8
Russell <russell@wyattdesign.works>
2026-03-23 16:18:46 -0400

    chore(deps): update packages to latest & add Dependabot config
    - Update react/react-dom to ^19.2.4
    - Update typescript to ~5.9.3
    - Update @mdx-js/react, clsx, prism-react-renderer
    - Add .github/dependabot.yml for weekly automated updates
    - Migrate onBrokenMarkdownLinks to markdown.hooks (Docusaurus v4 compat)

--- site/docusaurus.config.ts
+++ site/docusaurus.config.ts
@@ -30,7 +30,6 @@ const config: Config = {
   projectName: repoName,
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -42,6 +41,9 @@ const config: Config = {
 
   markdown: {
     mermaid: true,
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
   },
 
   themes: ['@docusaurus/theme-mermaid'],
